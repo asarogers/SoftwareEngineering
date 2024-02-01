84d66020d4e3701e7bda6bac4ab864419ff3aa1b
Eyerusalem-hub <79621412+Eyerusalem-hub@users.noreply.github.com>
2024-02-01 09:59:00 -0600

    home page update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,6 @@ import axios from "../api/axios";
 function Home() {
   const [selectedOption, setSelectedOption] = useState(null);
 
-
   const cycleOptions = [
     { label: "Move Forward", value: "1" },
     { label: "Move Backward", value: "2" },
@@ -14,130 +13,97 @@ function Home() {
     { label: "Turn Right", value: "4" },
   ];
 
-  const sep = [
-    { label: "patton hall", value: "34.7826° N, 86.5702° W" },
-    { label: "Move Backward", value: "2" },
-    { label: "Turn Left", value: "3" },
-    { label: "Turn Right", value: "4" },
-  ];
   const handleSelectChange = (selectedOption) => {
     setSelectedOption(selectedOption);
   };
 
-  const onSubmit= ()=>{
-    console.log("clicked", selectedOption)
-    axios.post("/send-command", {command: selectedOption})
-    .then((response)=>{
-      console.log(response.data)
-    })
-  }
+  const onSubmit = () => {
+    if (selectedOption) {
+      console.log("clicked", selectedOption);
+      axios
+        .post("/send-command", { command: selectedOption })
+        .then((response) => {
+          console.log(response.data);
+        });
+    } else {
+      console.log("Please select an option.");
+    }
+  };
 
   return (
     <div className="App">
       <Navbar />
       <div className="home-body">
-        <div className="Pick-Up Location">
-          Pick-Up Location
-          <div className="control-select">
-            <Select
-              options={cycleOptions}
-              onChange={handleSelectChange}
-              // Add custom styles as needed
-              styles={{
-                control: (provided, state) => ({
-                  ...provided,
-                  border: "1px solid #ccc",
-                  borderRadius: "4px",
-                  boxShadow: state.isFocused
-                    ? "0 0 0 2px rgba(0, 123, 255, 0.6)"
-                    : "none",
-                }),
-                option: (provided, state) => ({
-                  ...provided,
-                  backgroundColor: state.isSelected ? "#007BFF" : "white",
-                  color: state.isSelected ? "white" : "black",
-                }),
-              }}
-            />
-            <button
-              className="start-create start-button"
-              onClick={() => {
-                onSubmit();
-              }}
-            >
-              Start
-            </button>
+        <div className="home-container">
+          <div className="home-form">
+            <div className="pick-up-location">
+              <strong style={{ color: "white" }}>Pick-Up Location</strong>
+              <div className="control-select">
+                <Select options={cycleOptions} onChange={handleSelectChange} />
+              </div>
+            </div>
+            <br />
+            <div className="item-selection">
+              <strong style={{ color: "white" }}>Item Selection</strong>
+              <br />
+              <div className="control-select">
+                <Select
+                  options={cycleOptions}
+                  onChange={handleSelectChange}
+                  styles={{
+                    control: (provided, state) => ({
+                      ...provided,
+                      border: "1px solid #ccc",
+                      borderRadius: "4px",
+                      boxShadow: state.isFocused
+                        ? "0 0 0 2px rgba(0, 123, 255, 0.6)"
+                        : "none",
+                    }),
+                    option: (provided, state) => ({
+                      ...provided,
+                      backgroundColor: state.isSelected ? "#007BFF" : "white",
+                      color: state.isSelected ? "white" : "black",
+                    }),
+                  }}
+                />
+              </div>
+            </div>
+            <br />
+            <div className="drop-off-location">
+              <strong style={{ color: "white" }}>Drop-Off Location</strong>
+              <div className="control-select">
+                <Select
+                  options={cycleOptions}
+                  onChange={handleSelectChange}
+                  styles={{
+                    control: (provided, state) => ({
+                      ...provided,
+                      border: "1px solid #ccc",
+                      borderRadius: "4px",
+                      boxShadow: state.isFocused
+                        ? "0 0 0 2px rgba(0, 123, 255, 0.6)"
+                        : "none",
+                    }),
+                    option: (provided, state) => ({
+                      ...provided,
+                      backgroundColor: state.isSelected ? "#007BFF" : "white",
+                      color: state.isSelected ? "white" : "black",
+                    }),
+                  }}
+                />
+                <br />
+                <button
+                  className="start-create start-button"
+                  onClick={() => {
+                    onSubmit();
+                  }}
+                >
+                  Start
+                </button>
+              </div>
+            </div>
           </div>
         </div>
-
-        <div className="Item Selection">
-          Item Selection
-          <div className="control-select">
-            <Select
-              options={cycleOptions}
-              onChange={handleSelectChange}
-              // Add custom styles as needed
-              styles={{
-                control: (provided, state) => ({
-                  ...provided,
-                  border: "1px solid #ccc",
-                  borderRadius: "4px",
-                  boxShadow: state.isFocused
-                    ? "0 0 0 2px rgba(0, 123, 255, 0.6)"
-                    : "none",
-                }),
-                option: (provided, state) => ({
-                  ...provided,
-                  backgroundColor: state.isSelected ? "#007BFF" : "white",
-                  color: state.isSelected ? "white" : "black",
-                }),
-              }}
-            />
-            <button
-              className="start-create start-button"
-              onClick={() => {
-                onSubmit();
-              }}
-            >
-              Start
-            </button>
-          </div>
-        </div>
-
-        <div className="Drop-Off Location">
-          Drop-Off Location
-          <div className="control-select">
-            <Select
-              options={cycleOptions}
-              onChange={handleSelectChange}
-              // Add custom styles as needed
-              styles={{
-                control: (provided, state) => ({
-                  ...provided,
-                  border: "1px solid #ccc",
-                  borderRadius: "4px",
-                  boxShadow: state.isFocused
-                    ? "0 0 0 2px rgba(0, 123, 255, 0.6)"
-                    : "none",
-                }),
-                option: (provided, state) => ({
-                  ...provided,
-                  backgroundColor: state.isSelected ? "#007BFF" : "white",
-                  color: state.isSelected ? "white" : "black",
-                }),
-              }}
-            />
-            <button
-              className="start-create start-button"
-              onClick={() => {
-                onSubmit();
-              }}
-            >
-              Start
-            </button>
-          </div>
-        </div>
-
         <div className="map">map</div>
       </div>
     </div>
