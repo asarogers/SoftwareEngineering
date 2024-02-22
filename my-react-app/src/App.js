7781175bc204b0db7a2f180c556fa89c2a18171a
asa <cyberasasoftware@gmail.com>
2024-02-22 17:49:52 -0600

    stripped the library

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,15 +1,3 @@
-import {
-  Box,
-  Button,
-  ButtonGroup,
-  Flex,
-  HStack,
-  IconButton,
-  Input,
-  SkeletonText,
-  Text,
-} from '@chakra-ui/react'
-import { FaLocationArrow, FaTimes } from 'react-icons/fa'
 
 import { useJsApiLoader, GoogleMap} from '@react-google-maps/api'
 
@@ -19,65 +7,24 @@ const center = {lat: 48.854, lng: 2.2945}
 function App() {
 
 const {isLoaded} = useJsApiLoader({
-googleMapsApiKey:process.env.REACT_APP_GOOGLE_KEY,
+googleMapsApiKey:process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
 })
 
-console.log(process.env.REACT_APP_GOOGLE_KEY, "works")
+
 if(!isLoaded){
-  return <SkeletonText/>
+  return <>Loading</>
 }
 
   return (
-    <Flex
-      position='relative'
-      flexDirection='column'
-      alignItems='center'
-      
-      h='100vh'
-      w='100vw'
+    <div style={{position: "absolute", flexDirection:"column", alignItems: "center", height: "100vh", width: "100vw"}}
     >
-      <Box position='absolute' left={0} top={0} h='100%' w='100%'>
         {/* Google Maps Box*/ }
         <GoogleMap center={center} zoom={15} mapContainerStyle={{width: '100%', height:'100%'}}>
           {/* Display markers or directions*/ }
         </GoogleMap>
-      </Box>
 
-      <Box
-        p={4}
-        borderRadius='lg'
-        mt={4}
-        bgColor='white'
-        shadow='base'
-        minW='container.md'
-        zIndex='modal'
-      >
-        <HStack spacing={4}>
-          <Input type='text' placeholder='Origin' />
-          <Input type='text' placeholder='Destination' />
-          <ButtonGroup>
-            <Button colorScheme='pink' type='submit'>
-              Calculate Route
-            </Button>
-            <IconButton
-              aria-label='center back'
-              icon={<FaTimes />}
-              onClick={() => alert(123)}
-            />
-          </ButtonGroup>
-        </HStack>
-        <HStack spacing={4} mt={4} justifyContent='space-between'>
-          <Text>Distance: </Text>
-          <Text>Duration: </Text>
-          <IconButton
-            aria-label='center back'
-            icon={<FaLocationArrow />}
-            isRound
-            onClick={() => alert(123)}
-          />
-        </HStack>
-      </Box>
-    </Flex>
+
+    </div>
   )
 }
 
--- src/components/GoogleMapAPI.js
+++ src/components/GoogleMapAPI.js
@@ -1,39 +1,30 @@
-import {
-    Box,
-    Flex,
-  } from '@chakra-ui/react'
-   import { FaLocationArrow, FaTimes } from 'react-icons/fa'
-  
-   import { useJsApiLoader, GoogleMap} from '@react-google-maps/api'
+import { useJsApiLoader, GoogleMap} from '@react-google-maps/api'
   
   
   const center = {lat: 48.854, lng: 2.2945}
   
   function GoogleMapAPI() {
   
-  const {isLoaded} = useJsApiLoader({
-  googleMapsApiKey:process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
-  })
-  
-
-  
-    return (
-      <Flex
-        position='relative'
-        flexDirection='column'
-        alignItems='center'
-        
-        h='100vh'
-        w='100vw'
-      >
-        <Box position='absolute' left={0} top={0} h='100%' w='100%'>
-          {/* Google Maps Box*/ }
-          <GoogleMap center={center} zoom={15} mapContainerStyle={{width: '100%', height:'100%'}}>
-            {/* Display markers or directions*/ }
-          </GoogleMap>
-        </Box>
-      </Flex>
-    )
+    const {isLoaded} = useJsApiLoader({
+      googleMapsApiKey:process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+      })
+      
+      
+      if(!isLoaded){
+        return <>Loading</>
+      }
+      
+        return (
+          <div style={{position: "absolute", flexDirection:"column", alignItems: "center", height: "100vh", width: "100vw"}}
+          >
+              {/* Google Maps Box*/ }
+              <GoogleMap center={center} zoom={15} mapContainerStyle={{width: '100%', height:'100%'}}>
+                {/* Display markers or directions*/ }
+              </GoogleMap>
+      
+      
+          </div>
+        )
   }
   
   export default GoogleMapAPI
