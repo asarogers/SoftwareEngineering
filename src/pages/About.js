c4c2859f6a86af8ac4a5eda59ef8babdf8d3dba4
Eyerusalem-hub <79621412+Eyerusalem-hub@users.noreply.github.com>
2024-03-29 17:03:48 -0500

    admin page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,21 +1,43 @@
 import Navbar from "../components/Navbar";
-
+import robot from "../components/imgs/Delivery_robot_pic.jpg";
 function About() {
     return (
       <div className="About_body">
         <Navbar />
-        <h2><u>About</u></h2>
+        <div className="about-image-container">
+          <img 
+          className="about-image" 
+          src={robot} 
+          alt="Robot Image" />
         
-       <div class="text-block">
-        <h1 style={{fontSize: "800%", textAlign: "center"}}> E  S  D  I R </h1>
-        <h2 style={{fontSize: "300%", textAlign: "center"}} >Electromechanical Self-Driven Intelligent Rover </h2>
-        
-        <p>ESDIR is a robot capable of efficiently maneuvering through a maze, avoiding obstacles and objects, while reaching its intended destination. </p>
-        <p>ESDIR is controlled through the use of a web application with robust user authentication features, which enable users to submit delivery requests. These locations come from a pre-defined list of campus buildings, each linked to specific GPS locations. Admins will have the ability to add GPS locations to the list making this product more efficient for users. </p>
-        <p>We have hopes that this project will challenge more engineers and developers to keep creating innovation giving life’s tasks more simplicity. ESDIR where Service is Sovereignty.</p>
+        </div>
+
+        <div class="text-block">
+          <h1 style={{ fontSize: "600%", textAlign: "center" }}> E S D I R </h1>
+          <h2 style={{ fontSize: "250%", textAlign: "center" }}>
+            Electromechanical Self-Driven Intelligent Rover{" "}
+          </h2>
+
+          <p>
+            ESDIR is a robot capable of efficiently maneuvering through a maze,
+            avoiding obstacles and objects, while reaching its intended
+            destination.{" "}
+          </p>
+          <p>
+            It is controlled through the use of a web application with robust
+            user authentication features, which enable users to submit delivery
+            requests. These locations come from a pre-defined list of campus
+            buildings, each linked to specific GPS locations. Admins will have
+            the ability to add GPS locations to the list making this product
+            more efficient for users.{" "}
+          </p>
+          <p>
+            We have hopes that this project will challenge more engineers and
+            developers to keep creating innovation giving life’s tasks more
+            simplicity. ESDIR where Service is Sovereignty.
+          </p>
+        </div>
       </div>
-      </div>
-      
     );
   }
   
