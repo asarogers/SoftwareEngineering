460caea233e0de7cbf3a3c2e73f9b442c20bf084
asa <cyberasasoftware@gmail.com>
2024-04-02 00:04:18 -0500

    done

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,45 +1,38 @@
 import Navbar from "../components/Navbar";
-import robot from "../components/imgs/Delivery_robot_pic.jpg";
+import robot from "../components/imgs/robot.png";
 function About() {
-    return (
-      <div className="About_body">
-        <Navbar />
-        <div className="about-image-container">
-          <img 
-          className="about-image" 
-          src={robot} 
-          alt="Robot Image" />
-        
-        </div>
+  return (
+    <div className="About-container">
+      <Navbar />
 
-        <div class="text-block">
-          <h1 style={{ fontSize: "600%", textAlign: "center" }}> E S D I R </h1>
-          <h2 style={{ fontSize: "250%", textAlign: "center" }}>
-            Electromechanical Self-Driven Intelligent Rover{" "}
-          </h2>
+      <h1 className="e-s-d-i-r">
+        E&nbsp;&nbsp; S&nbsp;&nbsp; D&nbsp;&nbsp; I&nbsp;&nbsp; R
+      </h1>
+      <h2 className="esdir-name">
+        Electromechanical Self-Driven Intelligent Rover
+      </h2>
 
-          <p>
-            ESDIR is a robot capable of efficiently maneuvering through a maze,
-            avoiding obstacles and objects, while reaching its intended
-            destination.{" "}
-          </p>
-          <p>
-            It is controlled through the use of a web application with robust
-            user authentication features, which enable users to submit delivery
-            requests. These locations come from a pre-defined list of campus
-            buildings, each linked to specific GPS locations. Admins will have
-            the ability to add GPS locations to the list making this product
-            more efficient for users.{" "}
-          </p>
-          <p>
-            We have hopes that this project will challenge more engineers and
-            developers to keep creating innovation giving life’s tasks more
-            simplicity. ESDIR where Service is Sovereignty.
-          </p>
+      <p className="ESDIR-description">
+        ESDIR is a robot capable of efficiently maneuvering through a maze,
+        avoiding obstacles and objects, while reaching its intended destination.
+      </p>
+
+      <div className="text-robot-container">
+        <div className="robot-container">
+          <img className="robot" src={robot} />
         </div>
+        <p className="esdir-text">
+          ESDIR is controlled through the use of a web application with robust
+          user authentication features, which enable users to submit delivery
+          requests. These locations come from a pre-defined list of campus
+          buildings, each linked to specific GPS locations. Admins will have the
+          ability to add GPS locations to the list making this product more
+          efficient for users.
+        </p>
       </div>
-    );
-  }
-  
-  export default About;
-  
+      <p className="div">ESDIR where Service is Sovereignty!</p>
+    </div>
+  );
+}
+
+export default About;
